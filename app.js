395efeb0151a1f9197f935611d938db43709ac05
raxemmohamed <raxemmohamed@gmail.com>
2020-01-03 09:42:20 -0600

    fixed deprication warning

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-express = require('express');
-
+const express = require('express');
 const app = express();
-
-//middleware
-app.use('/posts', () => {
-    console.log('This is middleware running');
-});
+const mongoose = require('mongoose');
 
 //routes
 app.get('/', (req,res) => {
@@ -16,6 +11,11 @@ app.get('/posts', (req,res) => {
     res.send('We are on post')
 });
 
+//connect to DB
+mongoose.connect('mongodb+srv://raxem:raxem@cluster0-uzhgf.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true , useUnifiedTopology: true }, () => 
+    console.log('connected to DB')
+);
+
 //how do we sart listening?
-app.listen(3000);
+app.listen(3000, () => console.log('running on port 3000'));
 
