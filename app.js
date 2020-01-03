b3928f42200f146dfaaed01d5c72953785f6933f
raxemmohamed <raxemmohamed@gmail.com>
2020-01-03 09:23:45 -0600

    added routes

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 
 //routes
 app.get('/', (req,res) => {
-    res.send('We are home')
-})
+    res.send('We are on home')
+});
+
+app.get('/posts', (req,res) => {
+    res.send('We are on post')
+});
 
 //how do we sart listening?
 app.listen(3000);
