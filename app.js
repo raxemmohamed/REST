2e5af0e5e9922ba8e64093731c7810b5c22c0a12
raxemmohamed <raxemmohamed@gmail.com>
2020-01-03 11:34:59 -0600

    REST

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config')
+
+//middleware
+app.use(cors());
 
 app.use(bodyParser.json());
 
