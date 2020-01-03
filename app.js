5f14f65a50eb3be92f7731264d218affa8709ec3
raxemmohamed <raxemmohamed@gmail.com>
2020-01-03 10:13:51 -0600

    adding DB to .env

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv/config')
+
+//import routes
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute);
+
 
 //routes
 app.get('/', (req,res) => {
@@ -12,7 +19,7 @@ app.get('/posts', (req,res) => {
 });
 
 //connect to DB
-mongoose.connect('mongodb+srv://raxem:raxem@cluster0-uzhgf.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true , useUnifiedTopology: true }, () => 
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true , useUnifiedTopology: true }, () => 
     console.log('connected to DB')
 );
 
